8dfdfa8da7ccc0dd806317bfb53ea7c1802896e5
Okonos <tepyzboj@gmail.com>
2017-08-04 12:18:41 +0200

    Implement data connection handling ls command

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func parseArgs() (addr string) {
@@ -33,6 +36,21 @@ func readInput(prompt string) (text string, err error) {
 	return
 }
 
+func parseHostPort(addr string) (*net.TCPAddr, error) {
+	start, end := strings.Index(addr, "(")+1, strings.Index(addr, ")")
+	addrBytes := strings.Split(addr[start:end], ",")
+	host := strings.Join(addrBytes[:4], ".")
+	var port int
+	if upperByte, err := strconv.Atoi(addrBytes[4]); err == nil {
+		port += upperByte * 256
+	}
+	if lowerByte, err := strconv.Atoi(addrBytes[5]); err == nil {
+		port += lowerByte
+	}
+	fullAddr := strings.Join([]string{host, strconv.Itoa(port)}, ":")
+	return net.ResolveTCPAddr("tcp", fullAddr)
+}
+
 func promptLoop() {
 	for {
 		cmd, err := readInput("ftp> ")
@@ -45,8 +63,10 @@ func promptLoop() {
 		}
 
 		switch cmd {
+		case "ls":
+			return
 		default:
-			fmt.Println("Not implemented")
+			fmt.Println("Invalid command")
 		}
 	}
 }
@@ -70,17 +90,17 @@ func main() {
 
 	bufReader := bufio.NewReader(cmdConn)
 	buf, _ := bufReader.ReadString('\n')
-	fmt.Println(buf)
+	fmt.Print(buf)
 
 	if name, err := readInput("Name: "); err == nil {
 		cmdConn.Write([]byte("USER " + name + "\r\n"))
 		buf, _ = bufReader.ReadString('\n')
-		fmt.Println(buf)
+		fmt.Print(buf)
 
 		if pass, err := readInput("Password: "); err == nil || err == io.EOF {
 			cmdConn.Write([]byte("PASS " + pass + "\r\n"))
 			buf, _ = bufReader.ReadString('\n')
-			fmt.Println(buf)
+			fmt.Print(buf)
 		} else {
 			log.Fatal(err)
 		}
@@ -91,6 +111,28 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+
+	cmdConn.Write([]byte("PASV\r\n"))
+	buf, _ = bufReader.ReadString('\n')
+	fmt.Print(buf)
+
+	dataTCPAddr, err := parseHostPort(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dataConn, err := net.DialTCP("tcp", nil, dataTCPAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmdConn.Write([]byte("LIST\r\n"))
+	buf, _ = bufReader.ReadString('\n')
+	fmt.Print(buf)
+	dataReader := bufio.NewReader(dataConn)
+	data, _ := ioutil.ReadAll(dataReader)
+	fmt.Print(string(data))
+	dataConn.Close() // defer
+	buf, _ = bufReader.ReadString('\n')
+	fmt.Print(buf)
 
 	promptLoop()
 }
